64d29b6cb9cbcca71bf8b151cd0dceaf48048205
Chettipellysrilatha <149823428+Chettipellysrilatha@users.noreply.github.com>
2025-12-06 22:21:58 +0530

    server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Root endpoint
+app.get("/", (req, res) => {
+  res.send("Hello from Private EC2 API");
+});
+
+// Health endpoint
+app.get("/health", (req, res) => {
+  res.status(200).send("ok");
+});
+
+// IMPORTANT: listen on 0.0.0.0, not 127.0.0.1
+const PORT = 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
